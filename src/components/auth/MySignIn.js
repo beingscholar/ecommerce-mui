4f3842a1e18f488832ee620012ac9d330aa898b2
A1M8QE66 <a1m8qe66@Shubham-Pandey.local>
2020-07-25 13:11:58 +0530

    Mobile Menu

--- src/components/auth/MySignIn.js
+++ src/components/auth/MySignIn.js
@@ -1,175 +1,173 @@
-import React, { useState } from 'react';
-import { Link as RouterLink, useHistory } from 'react-router-dom';
-import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
+import React, { useState } from "react";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 
-import Button from '@material-ui/core/Button';
-import { Link } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { useUser } from '../utilities/user';
+import Button from "@material-ui/core/Button";
+import { Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useUser } from "../utilities/user";
 
-const useStyles = makeStyles(theme => ({
-    toolbar: {
-        backgroundColor: '#e8e8e8',
-        fontFamily: 'Roboto, system-ui, sans-serif',
-        minHeight: 45
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    backgroundColor: "#e8e8e8",
+    fontFamily: "Roboto, system-ui, sans-serif",
+    minHeight: 45,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: "space-between",
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    overflowX: "auto",
+    minHeight: 100,
+    verticalAlign: "center",
+  },
+  toolbarLink: {
+    padding: theme.spacing(4),
+    flexShrink: 0,
+    // fontSize: '1rem',
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  navBar: {
+    padding: "15px 30px",
+  },
+  navbarLink: {
+    textTransform: "capitalize",
+    color: "#00a0ff",
+    display: "inline-flex",
+    alignItems: "center",
+    paddingRight: "20px",
+    textShadow: "1px 1px 1px #2d262642",
+  },
+  signupBtn: {
+    color: "#fff",
+    backgroundColor: "#00a0ff",
+    borderRadius: "40px",
+    boxSizing: "border-box",
+    padding: "15px 30px",
+    verticalAlign: "top",
+    textShadow: "1px 1px 1px #2d262642",
+  },
+  searchBtn: {
+    width: 50,
+    display: "inline-flex",
+    backgroundColor: "#505050",
+    color: "fff",
+  },
+  formContainer: {
+    display: "inline-flex",
+    alignItems: "center",
+  },
+  textField: {
+    borderRadius: 40,
+    position: "relative",
+    backgroundColor: theme.palette.common.white,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    width: 600,
+    padding: "10px 0px 10px 30px",
+  },
+  input: {
+    "&&&:before": {
+      borderBottom: "none",
     },
-    toolbarTitle: {
-        flex: 1
+    "&&:after": {
+      borderBottom: "none",
     },
-    toolbarSecondary: {
-        justifyContent: 'space-between',
-        borderBottom: `1px solid ${theme.palette.divider}`,
-        overflowX: 'auto',
-        minHeight: 100,
-        verticalAlign: 'center'
-    },
-    toolbarLink: {
-        padding: theme.spacing(4),
-        flexShrink: 0,
-        // fontSize: '1rem',
-        justifyContent: 'space-between',
-        alignContent: 'center'
-    },
-    navBar: {
-        padding: '15px 30px'
-    },
-    navbarLink: {
-        textTransform: 'capitalize',
-        color: '#00a0ff',
-        display: 'inline-flex',
-        alignItems: 'center',
-        paddingRight: '20px',
-        textShadow: '1px 1px 1px #2d262642'
-    },
-    signupBtn: {
-        color: '#fff',
-        backgroundColor: '#00a0ff',
-        borderRadius: '40px',
-        boxSizing: 'border-box',
-        padding: '15px 30px',
-        verticalAlign: 'top',
-        textShadow: '1px 1px 1px #2d262642'
-    },
-    searchBtn: {
-        width: 50,
-        display: 'inline-flex',
-        backgroundColor: '#505050',
-        color: 'fff'
-    },
-    formContainer: {
-        display: 'inline-flex',
-        alignItems: 'center'
-    },
-    textField: {
-        borderRadius: 40,
-        position: 'relative',
-        backgroundColor: theme.palette.common.white,
-        border: '1px solid #ced4da',
-        fontSize: 16,
-        width: 600,
-        padding: '10px 0px 10px 30px'
-    },
-    input: {
-        '&&&:before': {
-            borderBottom: 'none'
-        },
-        '&&:after': {
-            borderBottom: 'none'
-        }
-    }
+  },
 }));
 
 const MySignIn = () => {
-    const classes = useStyles();
-    const history = useHistory();
-    const { login } = useUser();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const classes = useStyles();
+  const history = useHistory();
+  const { login } = useUser();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    async function handleSignIn(username, password) {
-        try {
-            // wait to see if login was successful (we don't care about the return
-            // value here)
-            await login(username, password);
-            history.push('/');
-        } catch (err) {
-            // If an error occured, showcase the proper message (we customised the
-            // message ourselves in `UserProvider`'s code)
-            console.log(err);
-        }
+  async function handleSignIn(username, password) {
+    try {
+      // wait to see if login was successful (we don't care about the return
+      // value here)
+      await login(username, password);
+      history.push("/");
+    } catch (err) {
+      // If an error occured, showcase the proper message (we customised the
+      // message ourselves in `UserProvider`'s code)
+      console.log(err);
     }
+  }
 
-    return (
-        <section className='user-structure'>
-            <div className='wrapper'>
-                <p>
-                    New Member?{' '}
-                    <Link component={RouterLink} to='/signup'>
-                        Register
-                    </Link>{' '}
-                    here.
-                </p>
-                <div className='user-structure--box'>
-                    <h3>Welcome to Hetchly! Please Join.</h3>
-                    <ValidatorForm
-                        noValidate
-                        onSubmit={e => {
-                            e.preventDefault();
-                            handleSignIn(username, password);
-                        }}
-                    >
-                        <div className='form-group'>
-                            Email*
-                            <TextValidator
-                                autoComplete='off'
-                                variant='outlined'
-                                fullWidth
-                                required
-                                placeholder='Please enter your email'
-                                id='username'
-                                key='username'
-                                name='username'
-                                value={username}
-                                onChange={e => {
-                                    setUsername(e.target.value);
-                                }}
-                                type='text'
-                                validators={['required', 'isEmail']}
-                                errorMessages={[
-                                    'This field is required',
-                                    'Please enter a valid email'
-                                ]}
-                            />
-                            {/* <TextField
+  return (
+    <section className="user-structure">
+      <div className="wrapper">
+        <p>
+          New Member?{" "}
+          <Link component={RouterLink} to="/signup">
+            Register
+          </Link>{" "}
+          here.
+        </p>
+        <div className="user-structure--box">
+          <h3>Welcome to Hetchly! Please Join.</h3>
+          <ValidatorForm
+            noValidate
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSignIn(username, password);
+            }}
+          >
+            <div className="form-group">
+              <label>Email*</label>
+              <TextValidator
+                autoComplete="off"
+                variant="outlined"
+                fullWidth
+                required
+                placeholder="Please enter your email"
+                id="username"
+                key="username"
+                name="username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                type="text"
+                validators={["required", "isEmail"]}
+                errorMessages={[
+                  "This field is required",
+                  "Please enter a valid email",
+                ]}
+              />
+              {/* <TextField
                                 id='outlined-basic'
                                 label='Email'
                                 placeholder='Please enter your email...'
                                 variant='outlined'
                                 required
                             /> */}
-                        </div>
-                        <div className='form-group'>
-                            Password*
-                            <TextValidator
-                                autoComplete='current-password'
-                                fullWidth
-                                required
-                                variant='outlined'
-                                id='password'
-                                key='password'
-                                name='password'
-                                placeholder='Please enter your password'
-                                value={password}
-                                onChange={e => {
-                                    setPassword(e.target.value);
-                                }}
-                                type='password'
-                                validators={['required']}
-                                errorMessages={[
-                                    'please enter a valid password'
-                                ]}
-                            />
-                            {/* <TextField
+            </div>
+            <div className="form-group">
+              <label>Password*</label>
+              <TextValidator
+                autoComplete="current-password"
+                fullWidth
+                required
+                variant="outlined"
+                id="password"
+                key="password"
+                name="password"
+                placeholder="Please enter your password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                type="password"
+                validators={["required"]}
+                errorMessages={["please enter a valid password"]}
+              />
+              {/* <TextField
                                 id='outlined-basic'
                                 label='Password'
                                 variant='outlined'
@@ -177,23 +175,23 @@ const MySignIn = () => {
                                 type='password'
                                 required
                             /> */}
-                        </div>
-                        <div className='form-link'>
-                            <Link to='/'>Forgot Password?</Link>
-                        </div>
-                        <Button
-                            variant='contained'
-                            color='primary'
-                            disableElevation
-                            type='submit'
-                        >
-                            Login
-                        </Button>
-                    </ValidatorForm>
-                </div>
             </div>
-        </section>
-    );
+            <div className="form-link">
+              <Link to="/">Forgot Password?</Link>
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              disableElevation
+              type="submit"
+            >
+              Login
+            </Button>
+          </ValidatorForm>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MySignIn;
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,128 +1,203 @@
-import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import MediaQuery from "react-responsive";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { Link } from "@material-ui/core";
+import PersonOutlinedIcon from "@material-ui/icons/PersonOutlined";
+import PropTypes from "prop-types";
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import logo from "../../assets/img/logo.png";
+import { useUser } from "../utilities/user";
+import navIcon from "../../assets/img/menu.svg";
+import closeIcon from "../../assets/img/close.svg";
 
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import { Link } from '@material-ui/core';
-import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
-import PropTypes from 'prop-types';
-import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import logo from '../../assets/img/logo.png';
-import { useUser } from '../utilities/user';
+const Header = (props) => {
+  const { push } = useHistory();
+  const { user, logout } = useUser();
 
-const Header = props => {
-    const { push } = useHistory();
-    const { user, logout } = useUser();
+  const handleSignOut = async () => {
+    await logout().then(() => window.location.replace("/signin"));
+  };
+  console.log(user);
+  return (
+    <>
+      <MediaQuery minWidth={768}>
+        <div className="topbar">
+          <Container maxWidth="lg">
+            <ul>
+              <li>
+                <Link component={RouterLink} to="/">
+                  Sell on Hetchly
+                </Link>
+              </li>
+              <li>
+                <Link component={RouterLink} to="/">
+                  Track my order
+                </Link>
+              </li>
+              <li>
+                <Link component={RouterLink} to="/">
+                  Help Center
+                </Link>
+              </li>
+            </ul>
+          </Container>
+        </div>
+      </MediaQuery>
 
-    const handleSignOut = async () => {
-        await logout().then(() => window.location.replace('/signin'));
-    };
-    console.log(user);
-    return (
-        // <header className="primary-header">
-        //   <div className=""
-        // </header>
-        <>
-            <div className='topbar'>
-                <Container maxWidth='lg'>
-                    <ul>
-                        <li>
-                            <Link component={RouterLink} to='/'>
-                                Sell on Hetchly
-                            </Link>
-                        </li>
-                        <li>
-                            <Link component={RouterLink} to='/'>
-                                Track my order
-                            </Link>
-                        </li>
-                        <li>
-                            <Link component={RouterLink} to='/'>
-                                Help Center
-                            </Link>
-                        </li>
-                    </ul>
-                </Container>
+      {/* Add ".in" class to show */}
+      <MediaQuery maxWidth={768}>
+        <div className="mobile-menu">
+          <div className="mobile-menu--container">
+            <img
+              src={closeIcon}
+              className="close-icon"
+              width="16"
+              alt="Close"
+            />
+            <div className="wrap">
+              <div className="logo">
+                <img src={logo} alt="Logo" />
+              </div>
+              <ul className="links">
+                <li>
+                  <Link component={RouterLink} to="/">
+                    Sell on Hetchly
+                  </Link>
+                </li>
+                <li>
+                  <Link component={RouterLink} to="/">
+                    Track my order
+                  </Link>
+                </li>
+                <li>
+                  <Link component={RouterLink} to="/">
+                    Help Center
+                  </Link>
+                </li>
+              </ul>
             </div>
+            <ul>
+              <li>
+                {user ? (
+                  <Link component={RouterLink} to="/profile">
+                    <PersonOutlinedIcon />
+                    Profile
+                  </Link>
+                ) : (
+                  <Link component={RouterLink} to="/signin">
+                    <PersonOutlinedIcon />
+                    Login
+                  </Link>
+                )}
+              </li>
+              <li>
+                {user ? (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    onClick={() => {
+                      handleSignOut();
+                    }}
+                    // to="/signup"
+                  >
+                    Sign Out
+                  </Button>
+                ) : (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    onClick={() => push("/signup")}
+                    // to="/signup"
+                  >
+                    Sign up
+                  </Button>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </MediaQuery>
 
-            <header className='primary-header'>
-                <Container maxWidth='lg'>
-                    <div className='logo'>
-                        <Link component={RouterLink} to='/'>
-                            <img src={logo} alt='Logo' />
-                        </Link>
-                    </div>
+      <header className="primary-header">
+        <Container maxWidth="lg">
+          <div className="logo">
+            <MediaQuery maxWidth={768}>
+              <img src={navIcon} width="20" alt="Menu" />
+            </MediaQuery>
+            <Link component={RouterLink} to="/">
+              <img src={logo} alt="Logo" />
+            </Link>
+          </div>
 
-                    <div className='search-box'>
-                        <input
-                            type='text'
-                            placeholder='Search products &amp; brands...'
-                        />
-                        <Button
-                            variant='contained'
-                            color='secondary'
-                            disableElevation
-                        >
-                            <SearchIcon />
-                        </Button>
-                    </div>
+          <div className="search-box">
+            <input type="text" placeholder="Search products &amp; brands..." />
+            <Button variant="contained" color="secondary" disableElevation>
+              <SearchIcon />
+            </Button>
+          </div>
 
-                    <ul className='links'>
-                        <li>
-                            <Link component={RouterLink} to='/cart'>
-                                <ShoppingCartOutlinedIcon />
-                                Cart
-                            </Link>
-                        </li>
-                        <li>
-                            {user ? (
-                                <Link component={RouterLink} to='/profile'>
-                                    <PersonOutlinedIcon />
-                                    Profile
-                                </Link>
-                            ) : (
-                                <Link component={RouterLink} to='/signin'>
-                                    <PersonOutlinedIcon />
-                                    Login
-                                </Link>
-                            )}
-                        </li>
-                        <li>
-                            {user ? (
-                                <Button
-                                    variant='contained'
-                                    color='primary'
-                                    disableElevation
-                                    onClick={() => {
-                                        handleSignOut();
-                                    }}
-                                    // to="/signup"
-                                >
-                                    Sign Out
-                                </Button>
-                            ) : (
-                                <Button
-                                    variant='contained'
-                                    color='primary'
-                                    disableElevation
-                                    onClick={() => push('/signup')}
-                                    // to="/signup"
-                                >
-                                    Sign up
-                                </Button>
-                            )}
-                        </li>
-                    </ul>
-                </Container>
-            </header>
-        </>
-    );
+          <ul className="links">
+            <li>
+              <Link component={RouterLink} to="/cart">
+                <ShoppingCartOutlinedIcon />
+                Cart
+              </Link>
+            </li>
+            <MediaQuery minWidth={768}>
+              <li>
+                {user ? (
+                  <Link component={RouterLink} to="/profile">
+                    <PersonOutlinedIcon />
+                    Profile
+                  </Link>
+                ) : (
+                  <Link component={RouterLink} to="/signin">
+                    <PersonOutlinedIcon />
+                    Login
+                  </Link>
+                )}
+              </li>
+              <li>
+                {user ? (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    onClick={() => {
+                      handleSignOut();
+                    }}
+                    // to="/signup"
+                  >
+                    Sign Out
+                  </Button>
+                ) : (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    onClick={() => push("/signup")}
+                    // to="/signup"
+                  >
+                    Sign up
+                  </Button>
+                )}
+              </li>
+            </MediaQuery>
+          </ul>
+        </Container>
+      </header>
+    </>
+  );
 };
 
 Header.propTypes = {
-    sections: PropTypes.array,
-    title: PropTypes.string
+  sections: PropTypes.array,
+  title: PropTypes.string,
 };
 
 export default Header;
