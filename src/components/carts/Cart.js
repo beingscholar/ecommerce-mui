f695066c1fa655f9acc64037233930d0fc49d251
Mayank <mayankagarwal@Xavients-MacBook-Pro.local>
2020-08-12 19:58:22 +0530

    Fixed: Spelling, Wrong count Select all & Order Summary

--- src/components/carts/Cart.js
+++ src/components/carts/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import Checkbox from "@material-ui/core/Checkbox";
 import Card from "@material-ui/core/Card";
@@ -30,62 +30,44 @@ const CART_API_URL =
 const PRODUCT_API_URL =
   "http://myproject-alb-692769319.ap-southeast-1.elb.amazonaws.com/products";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    margin: "2em 1em"
-  },
-  cover: {
-    width: 151
-  },
-  content: {
-    flex: "1 0 auto"
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column"
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allChecked: true,
+      products: [],
+      cart: [],
+      quantity: 1
+    };
   }
-}));
 
-const Cart = () => {
-  const classes = useStyles();
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
-  const [quantity, setQuantity] = useState(1);
-  const [checked, setChecked] = React.useState(true);
-
-  const handleChange = event => {
-    setChecked(event.target.checked);
-  };
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-    checkedF: true,
-    checkedG: true
-  });
-  const handleChangehead = event => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
-
-  useEffect(() => {
+  componentDidMount() {
+    const that = this;
     trackPromise(
       fetch(CART_API_URL + "/test")
         .then(response => {
           return response.json();
         })
         .then(data => {
-          setCart(data.items);
+          that.setState({ cart: data.items });
           var cartArr = data.items;
           var promises = [];
-          cartArr.forEach(async function (item) {
-            console.log(item.product_id);
-            var promise = await fetch(PRODUCT_API_URL + "/" + item.product_id)
+          cartArr.forEach(async function (product) {
+            // console.log(product.product_id);
+            var promise = await fetch(
+              PRODUCT_API_URL + "/" + product.product_id
+            )
               .then(response => {
                 return response.json();
               })
               .then(data => {
-                data.products[0].quantity = item.quantity;
-                setProducts(products => [...products, data.products[0]]);
+                data.products[0].quantity = product.quantity;
+                data.products[0].isChecked = true;
+                data.products[0].netTotal =
+                  parseInt(product.quantity) * parseInt(data.products[0].price);
+                that.setState(prevState => ({
+                  products: [...prevState.products, data.products[0]]
+                }));
               })
               .catch(error => {
                 alert(error);
@@ -98,162 +80,218 @@ const Cart = () => {
           alert(error);
         })
     );
-  }, []);
-
-  var cartData = (
-    <div>
-      <Grid container spacing={2}>
-        <Card>
-          <CardContent>
-            <h1>Your shopping cart is empty</h1>
-
-            <Button variant="contained" color="primary" href="#">
-              Empty Cart
-            </Button>
-            <p>You may add items to your shopping cart here.</p>
-          </CardContent>
-        </Card>
-      </Grid>
-    </div>
-  );
-
-  var productsList = <div> </div>;
-  if (cart) {
-    productsList = products.map(product => {
-      return (
-        <Box
-          component="div"
-          className="primary-box cart-product-card"
-          key={product.productId}
-        >
-          <Box className="product-box">
-            <Box className="product-checkbox">
-              <Checkbox
-                defaultChecked
-                color="primary"
-                inputProps={{ "aria-label": "secondary checkbox" }}
-              />
-              <CardMedia title="Image title" image={product.imageUrl} />
-            </Box>
-            <CardContent>
-              <Box className="product-info-content">
-                <Typography component="h3">
-                  <Link
-                    component={RouterLink}
-                    to={"/products/" + product.productId}
-                  >
-                    {product.productName}
-                  </Link>
-                </Typography>
-                <Typography>{product.productDescription}</Typography>
-                <Typography component="h4">By {product.supplier}</Typography>
-              </Box>
-              <Box className="product-box-action">
-                <Typography component="h5">
-                  {product.currency} {product.price}
-                </Typography>
-                <Box component="div" className="icon-group">
-                  <FavoriteBorderOutlinedIcon />
-                  <DeleteOutlineIcon />
-                </Box>
-              </Box>
-              <Box component="div" className="quantity">
-                <NumberField
-                  onChange={setQuantity}
-                  value={quantity}
-                  minValue={0}
-                  maxValue={product.quantity}
-                />
-              </Box>
-            </CardContent>
-          </Box>
-        </Box>
-      );
-    });
-    cartData = <>{productsList}</>;
   }
 
-  // return <div>{cartData}</div>;
+  handleChange = e => {
+    let itemName = e.target.name;
+    let checked = e.target.checked;
+    this.setState(prevState => {
+      let { products, allChecked } = prevState;
+      if (itemName === "checkAll") {
+        allChecked = checked;
+        products = products.map(product => ({
+          ...product,
+          isChecked: checked
+        }));
+      } else {
+        products = products.map(product =>
+          product.productName === itemName
+            ? { ...product, isChecked: checked }
+            : product
+        );
+        allChecked = products.every(product => product.isChecked);
+      }
+      return { products, allChecked };
+    });
+  };
 
-  return (
-    <Box component="div" className="main-content">
-      <Container maxWidth="lg">
-        <Box component="div" className="cart-header-action">
+  numberWithCommas = x => {
+    return x
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  renderList = () => {
+    var cartData = (
+      <div>
+        <Grid container spacing={2}>
+          <Card>
+            <CardContent>
+              <Typography component="h1">
+                Your shopping cart is empty
+              </Typography>
+              <Button variant="contained" color="primary" href="#">
+                Empty Cart
+              </Button>
+              <Typography component="p">
+                You may add items to your shopping cart here.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </div>
+    );
+
+    if (this.state.cart) {
+      cartData = this.state.products.map(product => {
+        return (
+          <Box
+            component="div"
+            className="primary-box cart-product-card"
+            key={product.productId}
+          >
+            <Box className="product-box">
+              <Box className="product-checkbox">
+                <Checkbox
+                  color="primary"
+                  inputProps={{ "aria-label": "secondary checkbox" }}
+                  key={product.productId}
+                  name={product.productName}
+                  value={product.productName}
+                  checked={product.isChecked}
+                  onChange={this.handleChange}
+                />
+                <CardMedia title="Image title" image={product.imageUrl} />
+              </Box>
+              <CardContent>
+                <Box className="product-info-content">
+                  <Typography component="h3">
+                    <Link
+                      component={RouterLink}
+                      to={"/products/" + product.productId}
+                    >
+                      {product.productName}
+                    </Link>
+                  </Typography>
+                  <Typography>{product.productDescription}</Typography>
+                  <Typography component="h4">By {product.supplier}</Typography>
+                </Box>
+                <Box className="product-box-action">
+                  <Typography component="h5">
+                    {product.currency} {this.numberWithCommas(product.netTotal)}
+                  </Typography>
+                  <Box component="div" className="icon-group">
+                    <FavoriteBorderOutlinedIcon />
+                    <DeleteOutlineIcon />
+                  </Box>
+                </Box>
+                <Box component="div" className="quantity">
+                  <NumberField
+                    onChange={e => this.setState({ quantity: e })}
+                    value={product.quantity}
+                    minValue={0}
+                    maxValue={product.quantity}
+                  />
+                </Box>
+              </CardContent>
+            </Box>
+          </Box>
+        );
+      });
+    }
+
+    return cartData;
+  };
+  render() {
+    const { products, allChecked } = this.state;
+    const productCount = products.length;
+    const subTotalCount = products.filter(product => product.isChecked).length;
+
+    const subtotal = products
+      .filter(({ isChecked }) => isChecked === true)
+      .reduce(
+        (totalPrice, product) => totalPrice + parseInt(product.netTotal),
+        0
+      );
+    console.log("subtotal: ", subtotal);
+
+    return (
+      <Box component="div" className="main-content">
+        <Container maxWidth="lg">
+          <Box component="div" className="cart-header-action">
+            <Grid container>
+              <Grid item xs={12} lg={8}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="checkAll"
+                      color="primary"
+                      checked={allChecked}
+                      onChange={this.handleChange}
+                    />
+                  }
+                  label={`Select All (${productCount} Items(s))`}
+                />
+                <Button>
+                  <DeleteOutlineIcon />
+                  Delete All
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
           <Grid container>
             <Grid item xs={12} lg={8}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={state.checkedB}
-                    onChange={handleChangehead}
-                    name="checkedB"
-                    color="primary"
-                  />
-                }
-                label="Seletc All (2 Items(s))"
-              />
-              <Button>
-                <DeleteOutlineIcon />
-                Delete All
-              </Button>
+              {this.renderList()}
+            </Grid>
+
+            <Grid item xs={12} lg={4}>
+              <Box
+                component="div"
+                className="primary-box delivery-info-box checkout-info"
+              >
+                <Box className="delivey-box">
+                  <Typography component="h3">Location</Typography>
+                  <ul>
+                    <li>
+                      <LocationOnOutlinedIcon />
+                      Metro Manila Quezon City, Quezon City, Project 6
+                    </li>
+                  </ul>
+                </Box>
+
+                <Box className="warranty-box">
+                  <Typography component="h3">Order Summary</Typography>
+                  <ul className="order-summary">
+                    <li>
+                      <Typography component="span">
+                        Subtotal ({subTotalCount} items)
+                      </Typography>
+                      <Typography component="span">
+                        {this.numberWithCommas(subtotal)}
+                      </Typography>
+                    </li>
+                    <li>
+                      <Typography component="span">Shipping Fee</Typography>
+                      <Typography component="span">0.00</Typography>
+                    </li>
+                  </ul>
+                </Box>
+
+                <Typography className="order-total">
+                  <Typography>Total</Typography>
+                  <Typography component="strong">
+                    {this.numberWithCommas(subtotal)}
+                  </Typography>
+                </Typography>
+
+                <Button
+                  component={RouterLink}
+                  to="/checkout"
+                  variant="contained"
+                  color="primary"
+                  disableElevation
+                  fullWidth
+                >
+                  Proceed to Checkout
+                </Button>
+              </Box>
             </Grid>
           </Grid>
-        </Box>
-        <Grid container>
-          <Grid item xs={12} lg={8}>
-            {cartData}
-          </Grid>
-
-          <Grid item xs={12} lg={4}>
-            <Box
-              component="div"
-              className="primary-box delivery-info-box checkout-info"
-            >
-              <Box className="delivey-box">
-                <Typography component="h3">Location</Typography>
-                <ul>
-                  <li>
-                    <LocationOnOutlinedIcon />
-                    Metro Manila Quezon City, Quezon City, Project 6
-                  </li>
-                </ul>
-              </Box>
-
-              <Box className="warranty-box">
-                <Typography component="h3">Order Summary</Typography>
-                <ul className="order-summary">
-                  <li>
-                    <Typography component="span">Subtotal (2 items)</Typography>
-                    <Typography component="span">50,000</Typography>
-                  </li>
-                  <li>
-                    <Typography component="span">Shipping Fee</Typography>
-                    <Typography component="span">40.00</Typography>
-                  </li>
-                </ul>
-              </Box>
-
-              <Typography className="order-total">
-                <Typography>Total</Typography>
-                <Typography component="strong">50,040</Typography>
-              </Typography>
-
-              <Button
-                component={RouterLink}
-                to="/checkout"
-                variant="contained"
-                color="primary"
-                disableElevation
-                fullWidth
-              >
-                Proceed to Checkout
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  );
-};
+        </Container>
+      </Box>
+    );
+  }
+}
 
 export default Cart;
