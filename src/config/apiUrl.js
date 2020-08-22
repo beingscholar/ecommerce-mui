07b078e616316b7650c8510f9e56f2c087df812e
Mayank <mayankagarwal@Xavients-MacBook-Pro.local>
2020-08-22 21:05:57 +0530

    Customer Image Upload Prod URL

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,6 +1,7 @@
 /* LOCAL ENV */
-export const CUSTOMER_URL = "http://0.0.0.0:5000/customers";
-export const S3_BUCKET_URL = "https://react-customer-images.s3-ap-southeast-1.amazonaws.com"
+// export const CUSTOMER_URL = "http://0.0.0.0:5000/customers";
 
 /* PROD ENV */
-// export const CUSTOMER_URL = "http://myproject-alb-692769319.ap-southeast-1.elb.amazonaws.com/customers";
+export const CUSTOMER_URL = "http://myproject-alb-692769319.ap-southeast-1.elb.amazonaws.com/customers";
+
+export const S3_BUCKET_URL = "https://react-customer-images.s3-ap-southeast-1.amazonaws.com"
